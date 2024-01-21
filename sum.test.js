a8db51ca8d490b4bca91564e5090133ed4f021ac
Pranay Alone <64677840+palone12@users.noreply.github.com>
2024-01-21 23:28:01 +0530

    Minor Changes

--- sum.test.js
+++ sum.test.js
@@ -2,7 +2,7 @@ const sum = require("./sum");
 const objectTesting = require("./Obj");
 
 test("Test Case 1", () => {
-  expect(sum(10, 20)).toBe(30);
+  expect(sum(15, 15)).toBe(30);
 });
 test("Test Case 2", () => {
   expect(sum(200, 33)).toBe(233);
